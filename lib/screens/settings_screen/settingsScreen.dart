cbd263f53397d2bdcd3fe61607b1854ae02c8857
rez120 <r.pishkar121@gmail.com>
2021-05-06 22:17:20 +0430

    added change username and password to settings screen

--- lib/screens/settings_screen/settingsScreen.dart
+++ lib/screens/settings_screen/settingsScreen.dart
@@ -1,3 +1,4 @@
+import 'package:app/helpers/info.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -6,13 +7,58 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  String message = "dgfg";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.lightBlue,
-        width: 200.0,
-        height: 200.0,
+    TextEditingController _usernameController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    TextEditingController _passwordRepeatController = TextEditingController();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              Text("Settings: "),
+              Text("new username: "),
+              TextField(
+                controller: _usernameController,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_usernameController.text != "") {
+                      username = _usernameController.text;
+                      setState(() {
+                        message = "Settings successfully applied";
+                      });
+                    }
+                  },
+                  child: Text("Change Username")),
+              Text("Change Password"),
+              TextField(
+                controller: _passwordController,
+              ),
+              TextField(
+                controller: _passwordRepeatController,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_passwordController.text ==
+                            _passwordRepeatController.text &&
+                        _passwordController.text != "") {
+                      setState(() {
+                        password = _passwordRepeatController.text;
+                        message = "Settings successfully applied";
+                      });
+                    }
+                  },
+                  child: Text("Change Password")),
+              Text(
+                "$message",
+                style: TextStyle(color: Colors.green),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
