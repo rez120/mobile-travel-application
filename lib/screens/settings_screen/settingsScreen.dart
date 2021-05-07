f4eda49c2f032260ea2330c9153842117785abb6
rez120 <r.pishkar121@gmail.com>
2021-05-07 18:15:18 +0430

    finalized settings screen

--- lib/screens/settings_screen/settingsScreen.dart
+++ lib/screens/settings_screen/settingsScreen.dart
@@ -7,7 +7,7 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  String message = "dgfg";
+  String message = "";
   @override
   Widget build(BuildContext context) {
     TextEditingController _usernameController = TextEditingController();
@@ -15,48 +15,142 @@ class _SettingsScreenState extends State<SettingsScreen> {
     TextEditingController _passwordRepeatController = TextEditingController();
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Column(
-            children: [
-              Text("Settings: "),
-              Text("new username: "),
-              TextField(
-                controller: _usernameController,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_usernameController.text != "") {
-                      username = _usernameController.text;
-                      setState(() {
-                        message = "Settings successfully applied";
-                      });
-                    }
-                  },
-                  child: Text("Change Username")),
-              Text("Change Password"),
-              TextField(
-                controller: _passwordController,
-              ),
-              TextField(
-                controller: _passwordRepeatController,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_passwordController.text ==
-                            _passwordRepeatController.text &&
-                        _passwordController.text != "") {
-                      setState(() {
-                        password = _passwordRepeatController.text;
-                        message = "Settings successfully applied";
-                      });
-                    }
-                  },
-                  child: Text("Change Password")),
-              Text(
-                "$message",
-                style: TextStyle(color: Colors.green),
-              ),
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "Settings ",
+                    style:
+                        TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "New Username: ",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(18.0),
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[350],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        hintText: "Enter New Username",
+                        icon: Icon(
+                          Icons.person,
+                          color: Colors.grey,
+                        )),
+                    controller: _usernameController,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_usernameController.text != "") {
+                          username = _usernameController.text;
+                          setState(() {
+                            message = "Changes successfully applied";
+                          });
+                        }
+                      },
+                      child: Text("Change Username")),
+                ),
+                SizedBox(height: 18.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "Change Password",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(18.0),
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[350],
+                  ),
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        hintText: "Enter Current Password",
+                        icon: Icon(
+                          Icons.book,
+                          color: Colors.grey,
+                        )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(18.0),
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[350],
+                  ),
+                  child: TextField(
+                    controller: _passwordRepeatController,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        hintText: "Enter New Password",
+                        icon: Icon(
+                          Icons.book,
+                          color: Colors.grey,
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_passwordController.text ==
+                                _passwordRepeatController.text &&
+                            _passwordController.text != "") {
+                          setState(() {
+                            password = _passwordRepeatController.text;
+                            message = "Settings successfully applied";
+                          });
+                        }
+                      },
+                      child: Text("Change Password")),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: Text(
+                    "$message",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
